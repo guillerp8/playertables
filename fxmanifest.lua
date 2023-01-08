925ae8fa245c02aa470374028fa0d53a3b97217b
Robert Crawford <68483930+robert4049@users.noreply.github.com>
2023-01-08 11:10:50 -0600

    feat: dynamic job load on resource start 	modified:   Config.lua 	modified:   Server/Main.lua 	modified:   fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,4 +8,9 @@ author 'guillerp#1928'
 
 server_only 'true'
 
-server_scripts {'@es_extended/imports.lua', 'Config.lua', 'Server/Main.lua'}
+server_scripts {
+    '@es_extended/imports.lua', 
+    'Config.lua', 
+    'Server/Main.lua',
+    '@mysql-async/lib/MySQL.lua',
+}
